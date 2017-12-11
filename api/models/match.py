55bbc52702dd89f30352623c7afb2733f90aaf34
mattegab <matteo.gabrielli13@gmail.com>
2017-12-11 13:06:38 +0100

    up

---
+++ api/models/match.py
@@ -0,0 +1,22 @@
+from google.appengine.ext import ndb
+
+class Match(ndb.Model):
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+    match_datetime = ndb.DateTimeProperty(indexed=False) # dt_format = "%Y-%m-%d %H:%M"
+
+    league = ndb.StringProperty(indexed=False)
+    team_home = ndb.StringProperty(indexed=False)
+    team_away = ndb.StringProperty(indexed=False)
+
+    odd_home = ndb.FloatProperty(indexed=False)
+    odd_draw = ndb.FloatProperty(indexed=False)
+    odd_away = ndb.FloatProperty(indexed=False)
+
+    bookmaker = ndb.StringProperty(indexed=False)
+    bookmaker_country = ndb.StringProperty(indexed=False)
+
+    team_home_id = ndb.IntegerProperty(indexed=False)
+    team_away_id = ndb.IntegerProperty(indexed=False)
+
+    match_id = ndb.StringProperty(indexed=False) # eg. 1_2_20171221     (will be made of team_home_id, team_away_id, match_datetime)
--- api/models/search.py
+++ api/models/search.py
@@ -3,13 +3,3 @@ from google.appengine.ext import ndb
 class Search(ndb.Model):
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
-
-#     date insert in db
-#   match date
-#   team_home name
-#   team_away name
-#   odd_1
-#   odd_X
-#   odd_2
-#   bookmaker
-#   country
